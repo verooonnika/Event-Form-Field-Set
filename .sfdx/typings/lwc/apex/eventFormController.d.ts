902073a1392afd3986d0fff6a9bc5dca35f18424
verooonnika <veronikamail.21@gmail.com>
2020-11-25 13:01:34 +0300

    feat: add getting record field values for edit

--- .sfdx/typings/lwc/apex/eventFormController.d.ts
+++ .sfdx/typings/lwc/apex/eventFormController.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/eventFormController.getFields" {
   export default function getFields(): Promise<any>;
 }
+declare module "@salesforce/apex/eventFormController.getEventRecord" {
+  export default function getEventRecord(param: {eventId: any}): Promise<any>;
+}
--- force-app/main/default/lwc/eventForm/eventForm.js
+++ force-app/main/default/lwc/eventForm/eventForm.js
@@ -1,13 +1,15 @@
-import {LightningElement, wire, track} from 'lwc';
+import {LightningElement, wire, track, api} from 'lwc';
 import getFields from '@salesforce/apex/eventFormController.getFields';
+import getEventRecord from '@salesforce/apex/eventFormController.getEventRecord';
 
 export default class EventForm extends LightningElement {
 
     @track fieldsData=[];
+    @api recordId;
+    @track eventRecord;
 
     @wire(getFields) fields ({data, error}) {
         if(data) {
-            console.debug(data);
             data.forEach(field => {
                 this.fieldsData.push({label: field.label, apiName: field.apiName, type: field.lwcType});
             });
@@ -15,9 +17,29 @@ export default class EventForm extends LightningElement {
         else if(error) {
             console.log(error);
         }
+    } 
+
+    @wire(getEventRecord, {eventId: '$recordId'}) record ({data, error}) {
+        if (data) {
+            this.eventRecord = {...data};
+
     }
+}
 
     connectedCallback(){
-
+        //console.debug('15');
     }
+
+    get fieldsInfo(){  
+        if(this.fieldsData && this.eventRecord){
+            this.fieldsData.forEach(field => {
+                if(this.eventRecord[field.apiName] != undefined){
+                    field.value = this.eventRecord[field.apiName];
+                }
+        });
+    } 
+
+        return this.fieldsData;
+    }
+
 }
